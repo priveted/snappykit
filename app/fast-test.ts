80882e8a3b48318dc8e031e96883bd55201b250f
priveted <priveted@yandex.ru>
2026-05-27 18:44:10 +0400

    fix: handle whitespace in class arrays

--- app/fast-test.ts
+++ app/fast-test.ts
@@ -1,28 +1,14 @@
 import './app.css';
-import { addClass, append, hide, make, on, prepend, show, text } from '@/utils';
+import { addClass, removeClass, toggleClass } from '@/utils';
 
-const tHeader = make('div', (th) => {
-  const testWrap = make('div', (tw) => {
-    addClass(tw, 'snp-test-wrap');
-  });
+addClass(document.body, ['hello', ' world ', 'x c']);
+addClass(document.body, ' one ');
+addClass(document.body, [' one-arr ']);
+addClass(document.body, 'one-normal');
 
-  const tBtn = make('button', (btn) => {
-    text(btn, 'Show');
-    addClass(btn, 'demo-btn');
-    on(btn, 'click.s', () => {
-      show(testWrap);
-    });
-  });
-
-  const tBtnH = make('button', (btn) => {
-    text(btn, 'Hide');
-    addClass(btn, 'demo-btn');
-    on(btn, 'click.h', () => {
-      hide(testWrap);
-    });
-  });
-
-  append(th, [testWrap, tBtn, tBtnH]);
-});
-
-prepend(document.body, [tHeader]);
+setTimeout(() => {
+  toggleClass(document.body, ['hello', ' world ', 'x c']);
+  toggleClass(document.body, ' one ');
+  removeClass(document.body, [' one-arr ']);
+  removeClass(document.body, 'one-normal');
+}, 5000);
